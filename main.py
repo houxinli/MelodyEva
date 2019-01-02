a5865d4d0f0d4aab9c3f1918a1dde57ce5f18a1a
houxinli <houxinli@hotmail.com>
2019-01-02 23:55:01 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 from au import SimpleRecorder
 from au.pitch_analyzer import PitchAnalyzer
 import time
+import eel
 
 print ("pa_test.py __name__ :", __name__)
 if __name__ == '__main__':
 
+	eel.init('web')                     # Give folder containing web files
 
 	# 创建录音机
 	SR = SimpleRecorder()
@@ -15,17 +17,17 @@ if __name__ == '__main__':
 	# 注册分析器
 	SR.register(PA)
 
+@eel.expose
+def initRecorder():
+	global SR
 	SR.start()
 
-	sec = 0
-	while sec < 15:
-		score_pitch = PA.getScore()
-		print("sec: ", sec)
-		print("score_pitch:", score_pitch)
-		time.sleep(1)
-		sec += 1
-
+@eel.expose
+def stopRecorder():
+	global SR
 	SR.stop()
 	SR.join()
 
-	print('end')
+
+
+	eel.start('index.html', size=(1536, 864))    # Start
