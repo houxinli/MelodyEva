c82a15c31db4066ea624f34083390d08be245e73
houxinli <houxinli@hotmail.com>
2019-01-02 10:04:05 +0800

    Update pa_test.py

--- pa_test.py
+++ pa_test.py
@@ -1,4 +1,4 @@
-from au import SimpleRecorder
+from au import SimpleRecorder, VolumeAnalyzer
 from au.pitch_analyzer import PitchAnalyzer
 import time
 
@@ -11,17 +11,20 @@ if __name__ == '__main__':
 
 	# 创建分析器
 	PA = PitchAnalyzer(refresh_time = 1)
+	VA = VolumeAnalyzer(rec_time = 1)
 
 	# 注册分析器
 	SR.register(PA)
+	SR.register(VA)
 
 	SR.start()
 
 	sec = 0
 	while sec < 15:
-		score_pitch = PA.getScore()
-		print("sec: ", sec)
-		print("score_pitch:", score_pitch)
+		# score_pitch = PA.getScore()
+		volume = VA.get_volume ()
+		print("sec: ", sec, "volume: ", volume)
+		# print("score_pitch:", score_pitch)
 		time.sleep(1)
 		sec += 1
 
