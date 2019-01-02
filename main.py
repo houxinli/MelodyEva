b305261a1a17e1e1906b9ae6ee4d83fdbe1b7d8c
houxinli <houxinli@hotmail.com>
2019-01-02 17:12:16 +0800

    web

---
+++ main.py
@@ -0,0 +1,31 @@
+from au import SimpleRecorder
+from au.pitch_analyzer import PitchAnalyzer
+import time
+
+print ("pa_test.py __name__ :", __name__)
+if __name__ == '__main__':
+
+
+	# 创建录音机
+	SR = SimpleRecorder()
+
+	# 创建分析器
+	PA = PitchAnalyzer(refresh_time = 1)
+
+	# 注册分析器
+	SR.register(PA)
+
+	SR.start()
+
+	sec = 0
+	while sec < 15:
+		score_pitch = PA.getScore()
+		print("sec: ", sec)
+		print("score_pitch:", score_pitch)
+		time.sleep(1)
+		sec += 1
+
+	SR.stop()
+	SR.join()
+
+	print('end')
