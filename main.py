1677803432bb3abc84d60b53aa2b5753dda59a91
houxinli <houxinli@hotmail.com>
2019-01-06 19:24:01 +0800

    initRecorder

--- main.py
+++ main.py
@@ -1,9 +1,31 @@
 from au import SimpleRecorder
 from au.pitch_analyzer import PitchAnalyzer
+import threading
 import time
 import eel
 
 print ("pa_test.py __name__ :", __name__)
+
+@eel.expose
+def stopRecorder():
+	global SR
+	SR.stop()
+	SR.join()
+
+@eel.expose
+def initRecorder():
+	global SR
+	SR.start()
+	sec = 0
+	while sec < 15:
+		score_pitch = PA.getScore()
+		print("sec: ", sec)
+		print("score_pitch:", score_pitch)
+		time.sleep(1)
+		sec += 1
+	stopRecorder()
+
+
 if __name__ == '__main__':
 
 	eel.init('web')                     # Give folder containing web files
@@ -17,17 +39,6 @@ if __name__ == '__main__':
 	# 注册分析器
 	SR.register(PA)
 
-@eel.expose
-def initRecorder():
-	global SR
-	SR.start()
-
-@eel.expose
-def stopRecorder():
-	global SR
-	SR.stop()
-	SR.join()
-
-
 
 	eel.start('index.html', size=(1536, 864))    # Start
+# eel.start('realtime.html', size=(1536, 864))    # Start
